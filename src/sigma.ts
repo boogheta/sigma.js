c64be4f9842555ef275fef88faa7154854de188e
Alexis Jacomy <alexis.jacomy@gmail.com>
2022-10-21 11:06:54 +0200

    Starts adapting programs to new setting
    This commit breaks everything, because programs have not been ported
    yet.
    
    Details:
    - Changes things we pass to programs
    - Renames new setting

--- src/sigma.ts
+++ src/sigma.ts
@@ -1168,10 +1168,10 @@ export default class Sigma<GraphType extends Graph = Graph> extends TypedEventEm
         matrix: this.matrix,
         width: this.width,
         height: this.height,
-        ratio: this.camera.ratio,
-        correctionRatio: this.correctionRatio / this.camera.ratio,
-        scalingRatio: this.pixelRatio,
-        nodesSizeZoomAdjuster: this.settings.nodesSizeZoomAdjuster,
+        pixelRatio: this.pixelRatio,
+        zoomRatio: this.camera.ratio,
+        sizeRatio: this.settings.zoomToSizeRatioFunction(this.camera.ratio),
+        correctionRatio: this.correctionRatio,
       });
     }
   }
@@ -1240,7 +1240,6 @@ export default class Sigma<GraphType extends Graph = Graph> extends TypedEventEm
     const viewportDimensions = this.getDimensions();
     const graphDimensions = this.getGraphDimensions();
     const padding = this.getSetting("stagePadding") || 0;
-    const nodesSizeZoomAdjuster = this.getSetting("nodesSizeZoomAdjuster") || Math.sqrt;
     this.matrix = matrixFromCamera(cameraState, viewportDimensions, graphDimensions, padding);
     this.invMatrix = matrixFromCamera(cameraState, viewportDimensions, graphDimensions, padding, true);
     this.correctionRatio = getMatrixImpact(this.matrix, cameraState, viewportDimensions);
@@ -1255,10 +1254,10 @@ export default class Sigma<GraphType extends Graph = Graph> extends TypedEventEm
         matrix: this.matrix,
         width: this.width,
         height: this.height,
-        ratio: cameraState.ratio,
-        correctionRatio: this.correctionRatio / cameraState.ratio,
-        scalingRatio: this.pixelRatio,
-        nodesSizeZoomAdjuster: nodesSizeZoomAdjuster,
+        pixelRatio: this.pixelRatio,
+        zoomRatio: this.camera.ratio,
+        sizeRatio: this.settings.zoomToSizeRatioFunction(this.camera.ratio),
+        correctionRatio: this.correctionRatio,
       });
     }
 
@@ -1273,10 +1272,10 @@ export default class Sigma<GraphType extends Graph = Graph> extends TypedEventEm
           matrix: this.matrix,
           width: this.width,
           height: this.height,
-          ratio: cameraState.ratio,
-          correctionRatio: this.correctionRatio / cameraState.ratio,
-          scalingRatio: this.pixelRatio,
-          nodesSizeZoomAdjuster: nodesSizeZoomAdjuster,
+          pixelRatio: this.pixelRatio,
+          zoomRatio: this.camera.ratio,
+          sizeRatio: this.settings.zoomToSizeRatioFunction(this.camera.ratio),
+          correctionRatio: this.correctionRatio,
         });
       }
     }
@@ -1297,7 +1296,7 @@ export default class Sigma<GraphType extends Graph = Graph> extends TypedEventEm
    */
   private updateCachedValues(): void {
     const { ratio } = this.camera.getState();
-    const nodesSizeZoomAdjuster = this.getSetting("nodesSizeZoomAdjuster") || Math.sqrt;
+    const nodesSizeZoomAdjuster = this.getSetting("zoomToSizeRatioFunction") || Math.sqrt;
     this.cameraSizeRatio = nodesSizeZoomAdjuster(ratio);
   }
 
